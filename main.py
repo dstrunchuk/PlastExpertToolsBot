b931e4b61fd9ca653b798dd3773b21bf8ec6da3b
sell <sell@MacBook-Pro-sell.local>
2025-04-17 22:33:17 +0300

    Обновление

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 # === КОНФИГ ===
 BOT_TOKEN = "7500703930:AAFaxpYm7mcMYkosPz2Hru9uBYaMsyOD8xY"
-DEVELOPER_ID = [987664835]
+DEVELOPER_ID = []
 
 # === ЗАГРУЗКА JSON ===
 def load_json(path):
