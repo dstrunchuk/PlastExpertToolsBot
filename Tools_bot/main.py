700c164261a18d9753c888b1f591a317fa29e741
sell <sell@MacBook-Pro-sell.local>
2025-04-22 20:38:47 +0300

    Обновление

--- Tools_bot/main.py
+++ Tools_bot/main.py
@@ -44,7 +44,7 @@ async def show_foreman_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("📋 Мои инструменты", callback_data="my_tools")],
         [InlineKeyboardButton("🔍 Весь инструмент", callback_data="all_tools_0")],
-        [InlineKeyboardButton("📷 Сканировать QR", web_app=WebAppInfo(url="https://plast-expert-tools-bot.vercel.app/"))]
+        [InlineKeyboardButton("📷 Сканировать QR", web_app=WebAppInfo(url="https://plast-expert-tools-bot.vercel.app//"))]
     ]
     markup = InlineKeyboardMarkup(keyboard)
     if update.message:
