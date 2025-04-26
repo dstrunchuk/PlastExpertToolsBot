c337e0b152df787c8dc75aae1e0e146de6ff1ae7
sell <sell@MacBook-Pro-sell.local>
2025-04-26 12:14:31 +0300

    Обновление

--- Tools_bot/main.py
+++ Tools_bot/main.py
@@ -1,11 +1,16 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-from handlers.menu import find_tool_handler, all_tools_handler, my_tools_handler, add_tool_handler, export_all_handler
+from handlers.menu import (
+    find_tool_handler,
+    all_tools_handler,
+    my_tools_handler,
+    add_tool_handler,
+    export_all_handler,
+    admin_menu_handler,
+    show_main_menu
+)
 from handlers.start import start_command, handle_registration
-from handlers.tools import handle_tool_action
-from handlers.tools import process_tool_id
-from handlers.tools import handle_view_tool
-from handlers.menu import show_main_menu
+from handlers.tools import handle_tool_action, process_tool_id, handle_view_tool
 from dotenv import load_dotenv
 import os
 
@@ -15,21 +20,20 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 # Хендлеры
-
-
+app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CallbackQueryHandler(find_tool_handler, pattern="^find_tool$"))
 app.add_handler(CallbackQueryHandler(all_tools_handler, pattern="^all_tools$"))
 app.add_handler(CallbackQueryHandler(my_tools_handler, pattern="^my_tools$"))
 app.add_handler(CallbackQueryHandler(add_tool_handler, pattern="^add_tool$"))
 app.add_handler(CallbackQueryHandler(export_all_handler, pattern="^export_all$"))
-app.add_handler(CommandHandler("start", start_command))
+app.add_handler(CallbackQueryHandler(admin_menu_handler, pattern="^admin_menu$"))
+app.add_handler(CallbackQueryHandler(show_main_menu, pattern="^main_back$"))
+
 app.add_handler(CallbackQueryHandler(handle_view_tool, pattern="^view_tool:"))
 app.add_handler(CallbackQueryHandler(handle_registration, pattern="^(register:|skip_admin)$"))
-app.add_handler(CallbackQueryHandler(handle_tool_action, pattern="^(take|transfer|store|request):"))
-app.add_handler(CallbackQueryHandler(show_main_menu, pattern="^main_back$"))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, process_tool_id))
-app.add_handler(CallbackQueryHandler(handle_registration, pattern="^register:"))
 app.add_handler(CallbackQueryHandler(handle_tool_action, pattern="^(take|store|request|transfer|assign|export|confirm_transfer|confirm_assign):"))
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, process_tool_id))
 
 # Запуск бота
 print("Бот запущен.")
