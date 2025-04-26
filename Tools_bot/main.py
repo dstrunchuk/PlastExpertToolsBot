6b1c7c5ef5d1d5a110830009aad6846ecd080cba
sell <sell@MacBook-Pro-sell.local>
2025-04-26 12:10:07 +0300

    Обновление

--- Tools_bot/main.py
+++ Tools_bot/main.py
@@ -1,5 +1,6 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from handlers.menu import find_tool_handler, all_tools_handler, my_tools_handler, add_tool_handler, export_all_handler
 from handlers.start import start_command, handle_registration
 from handlers.tools import handle_tool_action
 from handlers.tools import process_tool_id
@@ -14,6 +15,13 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 # Хендлеры
+
+
+app.add_handler(CallbackQueryHandler(find_tool_handler, pattern="^find_tool$"))
+app.add_handler(CallbackQueryHandler(all_tools_handler, pattern="^all_tools$"))
+app.add_handler(CallbackQueryHandler(my_tools_handler, pattern="^my_tools$"))
+app.add_handler(CallbackQueryHandler(add_tool_handler, pattern="^add_tool$"))
+app.add_handler(CallbackQueryHandler(export_all_handler, pattern="^export_all$"))
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CallbackQueryHandler(handle_view_tool, pattern="^view_tool:"))
 app.add_handler(CallbackQueryHandler(handle_registration, pattern="^(register:|skip_admin)$"))
