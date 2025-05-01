52f3e85bbd56ecfe8de16cb17caacafd57a0bfe0
sell <sell@MacBook-Pro-sell.local>
2025-05-01 22:12:16 +0300

    Обновление

--- Tools_bot/handlers/database.py
+++ Tools_bot/handlers/database.py
@@ -1,11 +1,10 @@
-from supabase import create_client, Client
 import os
+from supabase import create_client, AsyncClient
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
+supabase: AsyncClient = create_client(SUPABASE_URL, SUPABASE_KEY, is_async=True)
 # Получение всех инструментов
 async def get_all_tools():
     print("[DB] get_all_tools вызван")
