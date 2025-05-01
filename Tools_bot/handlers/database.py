2d7bc6e4f221c19f8e17bad3db34808ad9634e36
sell <sell@MacBook-Pro-sell.local>
2025-05-01 21:42:37 +0300

    Обновление

--- Tools_bot/handlers/database.py
+++ Tools_bot/handlers/database.py
@@ -1,5 +1,5 @@
-from supabase import create_client, AsyncClient
 import os
+from supabase import create_client, AsyncClient
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
