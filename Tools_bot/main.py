90b8857d3fe91d39ecf13609461223ac49679c96
sell <sell@MacBook-Pro-sell.local>
2025-05-01 20:53:28 +0300

    Обновление

--- Tools_bot/main.py
+++ Tools_bot/main.py
@@ -69,6 +69,6 @@ print("Бот запущен через WebHook.")
 app.run_webhook(
     listen="0.0.0.0",
     port=int(os.getenv("PORT", 8080)),
-    webhook_url=WEBHOOK_URL,
-    webhook_path="/webhook"
+    url_path="/webhook",
+    webhook_url=os.getenv("WEBHOOK_URL")
 )
